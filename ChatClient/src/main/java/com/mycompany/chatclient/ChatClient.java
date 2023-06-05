07865bf2d728011cc385fd888dd9f61489a33e4e
Bahadır Sina Terzioğlu <bahadir.terzioglu@stu.fsm.edu.tr>
2023-06-05 16:22:43 +0300

    Add Client and Server

---
+++ ChatClient/src/main/java/com/mycompany/chatclient/ChatClient.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chatclient;
+
+/**
+ *
+ * @author bahad
+ */
+public class ChatClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
